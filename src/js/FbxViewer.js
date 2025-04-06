4834b76fc38cd3e2e49b7da2ebe402b16628b775
AmelRidzal <amelridzal37@gmail.com>
2025-04-06 12:57:22 +0200

    FBX dummy models

--- src/js/FbxViewer.js
+++ src/js/FbxViewer.js
@@ -11,16 +11,16 @@ function Model({ modelPath }) {
 }
 
 const FbxViewer = () => {
-  const [model, setModel] = useState('/models/FBXTest.glb'); // default model path
+  const [model, setModel] = useState('/models/aero.glb'); // default model path
 
   return (
     <>
       {/* Custom Navbar */}
       <nav className="navbar">
         <ul className="nav-links">
-          <li><button onClick={() => setModel('/models/FBXTest.glb')}>Engine</button></li>
-          <li><button onClick={() => setModel('/models/FBXTest1.glb')}>Chassie</button></li>
-          <li><button onClick={() => setModel('/models/FBXTest.glb')}>Aero</button></li>
+        <li><button onClick={() => setModel('/models/aero.glb')}>Aero</button></li>
+          <li><button onClick={() => setModel('/models/chassie.glb')}>Chassie</button></li>
+          <li><button onClick={() => setModel('/models/engine.glb')}>Engine</button></li>
         </ul>
       </nav>
 
@@ -28,7 +28,7 @@ const FbxViewer = () => {
       <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: "absolute"}}>
         <color attach="background" args={["#101010"]} />
         <Suspense fallback={null}>
-          <PresentationControls speed={1.5} global zoom={0.5} >
+          <PresentationControls  key={model}  speed={1.5} global zoom={0.5}  polar={[-Math.PI / 2, Math.PI / 2]}>
             <Stage environment="sunset">
               <Model modelPath={model} />
             </Stage>
