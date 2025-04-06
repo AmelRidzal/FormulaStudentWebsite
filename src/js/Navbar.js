8bcc254ff1e99848dcd8db81d5439ec6c73a7b1f
AmelRidzal <amelridzal37@gmail.com>
2025-04-06 12:16:35 +0200

    FBXViewer update

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -1,16 +1,12 @@
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
-import FbxViewer from '../js/FbxViewer';
-import Default from './Default.js'
-import Picturs from './Picturs.js'
-import Update from './Updates.js'
 
 const Navbar = () => {
     return (
       <nav className="navbar">
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/upd">Update</Link></li>
+          <li><Link to="/upd">Updates</Link></li>
           <li><Link to="/pics">Picturs</Link></li>
           <li><Link to="/fbx">3D Model</Link></li>
         </ul>
