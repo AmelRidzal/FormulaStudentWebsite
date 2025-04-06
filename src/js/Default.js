98d47329895f47b2a5ef64e6d7dff04339b138e8
jasarevicedis <e.jasarevic03@gmail.com>
2025-04-06 15:41:48 +0200

    update sponsors on home

--- src/js/Default.js
+++ src/js/Default.js
@@ -22,6 +22,18 @@ const slides = [
 ];
 
 
+const sponsors = [
+  "https://1000logos.net/wp-content/uploads/2020/08/SolidWorks-Logo.png",
+  "https://1000logos.net/wp-content/uploads/2021/04/Pirelli-logo.png",
+  "https://1000logos.net/wp-content/uploads/2020/08/SolidWorks-Logo.png",
+  "https://1000logos.net/wp-content/uploads/2020/08/SolidWorks-Logo.png",
+  "https://1000logos.net/wp-content/uploads/2020/08/SolidWorks-Logo.png",
+  "https://1000logos.net/wp-content/uploads/2020/08/SolidWorks-Logo.png",
+];
+
+const sponsorLogos = [...sponsors, ...sponsors];
+
+
 function Home() {
   return (
     <div className="App">
@@ -43,8 +55,135 @@ function Home() {
         </div>
         ))}
         </section>
-        <section id="about-section">
-          <p>ABOUT</p>
+        <section className="about-section">
+          <div className="image-container">
+            <img src="https://www.estacaformulateam.com/wp-content/uploads/2024/12/20240803_FSS_TEAM_KKoloczek_073-1-1536x1152.jpg" alt="Seagulls Lübeck Team" />
+          </div>
+          <div className="text-container">
+            <h3 className="subtitle">University of Sarajevo</h3>
+            <h2 className="title">
+              UNSA <span className="highlight">RACING</span>
+            </h2>
+            <p className="description">
+              Our team, the "UNSA RACING" of the University of Sarajevo, was founded in February 2025. Since then, our team has grown to around 70 members, representing a wide variety of disciplines, including mechanical engineering, computer science, electrical engineering, industrial engineering, business administration, information technology and design, biomedical engineering, architecture, and hearing acoustics. Our primary goal is to build a new racing car for the upcoming competitions. This car will be built according to the specifications of "Formula Student," the largest international design competition for students. We are driven not only by our fascination with racing, but also by an incredible team spirit, which contributes significantly to the individual development of each team member and their development into true experts in their field.
+            </p>
+            <a href="#" className="more-link">
+              More about us →
+            </a>
+          </div>
+        </section>
+        
+        <section id="sponsors-section">
+          <h2 className="sponsors-title">Our Sponsors</h2>
+          <div class="sponsor-slider-wrapper">
+            <div class="sponsor-slider slide-1">
+              <div class="item">
+                <a href="#">
+                  <img src="https://www.pngarts.com/files/3/Logo-PNG-Image-Background.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://logodownload.org/wp-content/uploads/2020/10/garena-logo.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://pngimg.com/uploads/nike/nike_PNG12.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="http://assets.stickpng.com/images/585990234f6ae202fedf28cf.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://logos-world.net/wp-content/uploads/2021/02/Sky-Logo.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://purepng.com/public/uploads/large/purepng.com-disney-logologobrand-logoiconslogos-251519939495wtv86.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Arena_of_Valor_logo.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://www.pngall.com/wp-content/uploads/2016/06/Superman-Logo-Free-Download-PNG.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://blackpinkupdate.com/wp-content/uploads/2020/06/BLACKPINK-Logo-PNG-Transparent-Black.png" alt="img"/>
+                </a>
+              </div>
+            </div>
+            <div class="sponsor-slider slide-2">
+              <div class="item">
+                <a href="#">
+                  <img src="https://www.pngarts.com/files/3/Logo-PNG-Image-Background.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://logodownload.org/wp-content/uploads/2020/10/garena-logo.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://pngimg.com/uploads/nike/nike_PNG12.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="http://assets.stickpng.com/images/585990234f6ae202fedf28cf.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://logos-world.net/wp-content/uploads/2021/02/Sky-Logo.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://purepng.com/public/uploads/large/purepng.com-disney-logologobrand-logoiconslogos-251519939495wtv86.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Arena_of_Valor_logo.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://www.pngall.com/wp-content/uploads/2016/06/Superman-Logo-Free-Download-PNG.png" alt="img"/>
+                </a>
+              </div>
+              <div class="item">
+                <a href="#">
+                  <img src="https://blackpinkupdate.com/wp-content/uploads/2020/06/BLACKPINK-Logo-PNG-Transparent-Black.png" alt="img"/>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id='team-section'>
+          <p>TEAM SEKCIJA EDIS</p>
         </section>
         <section id="updates-section">
           <div className='updates-header'>
@@ -73,8 +212,8 @@ function Home() {
             />
           </div>
         </section>
-        <section id="contact-section">
-          <p>CONTACT</p>
+        <section id='contact-section'>
+          <p>Contact</p>
         </section>
       </main>
     </div>
