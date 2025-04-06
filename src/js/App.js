ec5a99d88dc40b0a4925b1f229ffa7e8280581ce
AmelRidzal <amelridzal37@gmail.com>
2025-04-06 11:43:54 +0200

    New pages

--- src/js/App.js
+++ src/js/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import FbxViewer from '../js/FbxViewer';
-import Test from '../js/Test.js'
+import Default from './Default.js'
+import Picturs from './Picturs.js'
+import Update from './Updates.js'
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/fbx" element={<FbxViewer />} />
-      <Route path="/" element={<Test />} />
+      <Route path="/" element={<Default />} />
+      <Route path="/pics" element={<Picturs />} />
+      <Route path="/upd" element={<Update />} />
         {/* other routes here */}
       </Routes>
     </Router>
