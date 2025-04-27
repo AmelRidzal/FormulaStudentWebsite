ac16105aa9cbb8e67010f1801037d9d97965728d
AmelRidzal <amelridzal37@gmail.com>
2025-04-27 02:10:28 +0200

    added backend

--- frontend/src/js/Picturs.js
+++ frontend/src/js/Picturs.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import '../css/Collors.css';
 import '../css/Pictures.css';
 
@@ -7,11 +6,13 @@ const Pictures = () => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    const backendURL = process.env.BACKEND_URL;
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+    console.log(backendURL);
 
     fetch(`${backendURL}/api/gallery`)
       .then(res => res.json())
       .then(data => {
+        console.log('Fetched images:', data);
         if (Array.isArray(data)) {
           setImages(data);
         } else {
@@ -21,11 +22,19 @@ const Pictures = () => {
       .catch(err => console.error("Failed to load images", err));
   }, []);
 
+  const formatUrl = (src) => {
+    if (src.startsWith('http')) {
+      return src;
+    } else {
+      return `https://${src}`;
+    }
+  };
+
   return (
     <div className="gallery-container">
       {images.map((src, index) => (
         <div key={index} className="gallery-item">
-          <img src={src} alt={`Gallery ${index + 1}`} />
+          <img src={formatUrl(src)} alt={`Gallery ${index + 1}`} />
         </div>
       ))}
     </div>
